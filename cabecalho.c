80b1c5adb59eb5e4c14b79fe4600717fee493ec2
miguel sena <miguelsenacardoso00@gmail.com>
2025-08-20 20:13:09 -0300

    Create cabecalho.c

---
+++ cabecalho.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ 
+ 
+int main() {
+    int matricula = 2024001234;          // exemplo de matrícula
+    float altura = 1.75;                 // exemplo de altura em metros
+    float peso = 70.456;                 // exemplo de peso em kg 
+ printf("\nDisciplina: Algoritmo de programação\n");
+    printf("Turma: GPE172024\n");
+    printf("Nome: Miguel Sena Cardoso\n");
+    printf("Matrícula: %d\n", matricula);
+    printf("Altura: %.2f m\n", altura);   // 2 casas
+    printf("Peso: %.3f kg\n", peso);      // 3 casas
+}
